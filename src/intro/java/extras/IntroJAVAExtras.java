d320e692a3df342c1d02a6620e596a833a205616
Rebeca Heredia <rbkheredia90@gmail.com>
2023-06-07 20:17:35 -0300

    carga de numeros, ciclo while, do while

--- src/intro/java/extras/IntroJAVAExtras.java
+++ src/intro/java/extras/IntroJAVAExtras.java
@@ -5,6 +5,8 @@
  */
 package intro.java.extras;
 
+import java.util.Scanner;
+
 /**
  *
  * @author rebek
@@ -15,7 +17,61 @@ public class IntroJAVAExtras {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //ejercicio7
+//        Realice un programa que calcule y visualice el valor máximo, el valor mínimo y el promedio
+//de n números (n>0). El valor de n se solicitará al principio del programa y los números
+//serán introducidos por el usuario. Realice dos versiones del programa, una usando el
+//bucle “while” y otra con el bucle “do - while”.
+        Scanner read = new Scanner(System.in);
+        System.out.println("¿Cuantos numeros desea introducir?");
+        int n = read.nextInt();
+        int i =0;
+        int num;
+        int total = 0;
+        int min = 50000000;
+        int max = -250000;
+        //version while
+//        while(i < n){
+//            
+//            System.out.println("Introduce un numero");
+//            num = read.nextInt();
+//            if(i==0){
+//                min=num;
+//                max=num;
+//            }
+//            total = total + num;
+//            i++;
+//            if(num<min){
+//                min = num;
+//            }
+//            if (num>max){
+//                max = num;
+//            }
+//        }
+//        System.out.println("Minimo: "+min);
+//        System.out.println("Maximo: "+max);
+//        System.out.println("Promedio: "+(total/n));
+        
+        //do while
+        do{
+            System.out.println("Introduce un numero");
+            num = read.nextInt();
+            if(i==0){
+                min=num;
+                max=num;
+            }
+            total = total + num;
+            i++;
+            if(num<min){
+                min = num;
+            }
+            if (num>max){
+                max = num;
+            }
+        }while(i<n);
+        System.out.println("Minimo: "+min);
+        System.out.println("Maximo: "+max);
+        System.out.println("Promedio: "+(total/n));
     }
     
 }
