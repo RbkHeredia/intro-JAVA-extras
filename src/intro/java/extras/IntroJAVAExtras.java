6d78948d9e711fdf948ca4a7bc3cc6bd39e92eb4
Rebeca Heredia <rbkheredia90@gmail.com>
2023-06-07 19:32:39 -0300

    numeros romanos

--- src/intro/java/extras/IntroJAVAExtras.java
+++ src/intro/java/extras/IntroJAVAExtras.java
@@ -5,6 +5,8 @@
  */
 package intro.java.extras;
 
+import java.util.Scanner;
+
 /**
  *
  * @author rebek
@@ -15,7 +17,50 @@ public class IntroJAVAExtras {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner read = new Scanner(System.in);
+        //ejercicio4
+//        Elaborar un algoritmo en el cuál se ingrese un número entre 1 y 10 y se muestre su
+//equivalente en romano
+        System.out.println("Ingrese un numero del 1 al 10");
+        int num = read.nextInt();
+        String romano="";
+        while(num>10 || num<1){
+            System.out.println("Numero equivocado, ingresa nuevamente");
+            num = read.nextInt();
+        }
+        switch(num){
+            case 1:
+                romano = "I";
+                break;
+            case 2:
+                romano = "II";
+                break;
+            case 3:
+                romano = "III";
+                break;
+            case 4:
+                romano = "IV";
+                break;
+            case 5:
+                romano = "V";
+                break;
+            case 6:
+                romano = "VI";
+                break;
+            case 7:
+                romano = "VII";
+                break;
+            case 8:
+                romano = "VIII";
+                break;
+            case 9:
+                romano = "IX";
+                break;
+            case 10:
+                romano = "X";
+                break;
+        }
+        System.out.println("El num "+num+ " es equivalente a "+ romano+ " en numeros romanos");
     }
     
 }
