829ee01d73bb6bd04aa1f4092b462626b5a61e90
Rebeca Heredia <rbkheredia90@gmail.com>
2023-06-07 19:10:03 -0300

    verificar si es vocal

--- src/intro/java/extras/IntroJAVAExtras.java
+++ src/intro/java/extras/IntroJAVAExtras.java
@@ -5,6 +5,8 @@
  */
 package intro.java.extras;
 
+import java.util.Scanner;
+
 /**
  *
  * @author rebek
@@ -15,7 +17,29 @@ public class IntroJAVAExtras {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //ejercicio3
+//        Elaborar un algoritmo en el cuál se ingrese una letra y se detecte si se trata de una vocal.
+//Caso contrario mostrar un mensaje. Nota: investigar la función equals() de la clase String
+    Scanner read = new Scanner(System.in);
+    String vocales[] = new String[5];
+    vocales[0] = "a";
+    vocales[1] = "e";
+    vocales[2] = "i";
+    vocales[3] = "o";
+    vocales[4] = "u";
+    int counter =0;
+        System.out.println("Ingresa una letra");
+        String letter = read.next();
+        for (int i = 0; i < 5; i++) {
+            if(vocales[i].equals(letter)){
+                counter ++;
+            } 
+        }
+        if(counter >0){
+            System.out.println("Es una vocal");
+        } else {
+            System.out.println("No es una vocal");
+        }
     }
     
 }
