ea05485cbc8c7570cfddd72a4ba3cc08725afe45
Rebeca Heredia <rbkheredia90@gmail.com>
2023-06-06 21:42:45 -0300

    minutos a dias y horas

--- src/intro/java/extras/IntroJAVAExtras.java
+++ src/intro/java/extras/IntroJAVAExtras.java
@@ -5,6 +5,8 @@
  */
 package intro.java.extras;
 
+import java.util.Scanner;
+
 /**
  *
  * @author rebek
@@ -15,7 +17,16 @@ public class IntroJAVAExtras {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // ej1
+//        Dado un tiempo en minutos, calcular su equivalente en días y horas. Por ejemplo, si el
+//usuario ingresa 1600 minutos, el sistema debe calcular su equivalente: 1 día, 2 horas.
+        Scanner read = new Scanner(System.in);
+        System.out.println("Ingrese la cantidad de minutos");
+        int minutes = read.nextInt();
+        int days = Math.round(minutes/1440) ;
+        int rest = minutes - (days*1440);
+        int hours = Math.round(rest/60);
+        System.out.println(minutes+ "minutos son: "+ days+" dias y " + hours + " horas");
     }
     
 }
