7101e0bf22587c1512c39e016ce1ac9d41087b51
Rebeca Heredia <rbkheredia90@gmail.com>
2023-06-08 19:59:05 -0300

    multiplicacion con numeros random

--- src/intro/java/extras/IntroJAVAExtras.java
+++ src/intro/java/extras/IntroJAVAExtras.java
@@ -5,6 +5,8 @@
  */
 package intro.java.extras;
 
+import java.util.Scanner;
+
 /**
  *
  * @author rebek
@@ -15,7 +17,25 @@ public class IntroJAVAExtras {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // ejercicio10
+//        Realice un programa para que el usuario adivine el resultado de una multiplicación entre
+//dos números generados aleatoriamente entre 0 y 10. El programa debe indicar al usuario
+//si su respuesta es o no correcta. En caso que la respuesta sea incorrecta se debe permitir
+//al usuario ingresar su respuesta nuevamente. Para realizar este ejercicio investigue como
+//utilizar la función Math.random() de Java
+        Scanner read = new Scanner(System.in);
+        int num1 = (int) (Math.random()*10);
+//        System.out.println(num1);
+        int num2 = (int) (Math.random()*10);
+//        System.out.println(num2);
+        int mult = num1 * num2;
+        System.out.println("Ingrese un numero");
+        int input = read.nextInt();
+        while(input != mult){
+            System.out.println("Respuesta incorrecta, intente nuevamente");
+            input = read.nextInt();
+        }
+        System.out.println("Acertaste!");
     }
     
 }
