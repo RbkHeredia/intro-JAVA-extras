6e70ebeefde7333beb06cdb8bc7c8d4b4284ab7c
Rebeca Heredia <rbkheredia90@gmail.com>
2023-06-08 20:06:03 -0300

    contador de digitos

--- src/intro/java/extras/IntroJAVAExtras.java
+++ src/intro/java/extras/IntroJAVAExtras.java
@@ -5,6 +5,8 @@
  */
 package intro.java.extras;
 
+import java.util.Scanner;
+
 /**
  *
  * @author rebek
@@ -15,7 +17,21 @@ public class IntroJAVAExtras {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //ejercicio11
+//        Escribir un programa que lea un número entero y devuelva el número de dígitos que
+//componen ese número. Por ejemplo, si introducimos el número 12345, el programa
+//deberá devolver 5. Calcular la cantidad de dígitos matemáticamente utilizando el operador
+//de división. Nota: recordar que las variables de tipo entero truncan los números o
+//resultados.
+        Scanner read = new Scanner(System.in);
+        System.out.println("Ingrese un numero");
+        int num = read.nextInt();
+        int dig = 0;
+        while(num>0){
+            num=num/10;
+            dig++;
+        }
+        System.out.println("El numero tiene "+dig+" digitos");
     }
     
 }
