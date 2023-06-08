1d528a35e7b46ad1192120d96b3e01900bdfa962
Rebeca Heredia <rbkheredia90@gmail.com>
2023-06-08 19:37:54 -0300

    pares e impares

--- src/intro/java/extras/IntroJAVAExtras.java
+++ src/intro/java/extras/IntroJAVAExtras.java
@@ -5,6 +5,8 @@
  */
 package intro.java.extras;
 
+import java.util.Scanner;
+
 /**
  *
  * @author rebek
@@ -15,7 +17,29 @@ public class IntroJAVAExtras {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //ejercicio8
+//        Escriba un programa que lea números enteros. Si el número es múltiplo de cinco debe
+//detener la lectura y mostrar la cantidad de números leídos, la cantidad de números pares y
+//la cantidad de números impares. Al igual que en el ejercicio anterior los números
+//negativos no deben sumarse. Nota: recordar el uso de la sentencia break.
+    Scanner read = new Scanner(System.in);
+    int num;
+    int pares=0;
+    int impares=0;
+    do{
+        System.out.println("Ingrese un numero");
+        num = read.nextInt();
+        if(num>0){
+            if(num%2 == 0){
+                pares++;
+            } else {
+                impares++;
+            }
+        }
+    }while(num%5 != 0);
+    
+        System.out.println("Pares: "+pares);
+        System.out.println("Impares: "+impares);
     }
     
 }
