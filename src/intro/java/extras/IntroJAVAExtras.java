c122c9ffb85f739e2b92d0bd162079b53e6c673e
Rebeca Heredia <rbkheredia90@gmail.com>
2023-06-08 20:28:58 -0300

    escalera de numeros

--- src/intro/java/extras/IntroJAVAExtras.java
+++ src/intro/java/extras/IntroJAVAExtras.java
@@ -5,6 +5,8 @@
  */
 package intro.java.extras;
 
+import java.util.Scanner;
+
 /**
  *
  * @author rebek
@@ -15,7 +17,21 @@ public class IntroJAVAExtras {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // ejercicio13
+//        Crear un programa que dibuje una escalera de números, donde cada línea de números
+//comience en uno y termine en el número de la línea. Solicitar la altura de la escalera al
+//usuario al comenzar. Ejemplo: si se ingresa el número 3:
+//1
+//12
+//123
+        Scanner read = new Scanner(System.in);
+        System.out.println("Cuantos numeros desea mostrar en la escalera?");
+        int num = read.nextInt();
+        String res = "";
+        for (int i = 1; i <= num; i++) {
+            res = res + String.valueOf(i);
+            System.out.println(res);
+        }
     }
     
 }
