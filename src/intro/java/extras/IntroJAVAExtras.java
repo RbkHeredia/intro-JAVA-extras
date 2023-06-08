d61799229792090f2fcc1607d1a2faac2649464a
Rebeca Heredia <rbkheredia90@gmail.com>
2023-06-08 20:16:53 -0300

    pasar de int a string

--- src/intro/java/extras/IntroJAVAExtras.java
+++ src/intro/java/extras/IntroJAVAExtras.java
@@ -15,7 +15,37 @@ public class IntroJAVAExtras {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // ejercicio12
+//        Necesitamos mostrar un contador con 3 dígitos (X-X-X), que muestre los números del 0-0-
+//0 al 9-9-9, con la particularidad que cada vez que aparezca un 3 lo sustituya por una E.
+//Ejemplo:
+//0-0-0
+//0-0-1
+//0-0-2
+//0-0-E
+//0-0-4
+//0-1-2
+//0-1-E
+//Nota: investigar función equals() y como convertir números a String.
+        for (int i = 0; i < 10; i++) {
+            String first = String.valueOf(i);
+            if(first.equals("3")){
+               first = "E";
+            }
+            for (int j = 0; j < 10; j++) {
+                String second = String.valueOf(j);
+                if(second.equals("3")){
+                        second = "E";
+                    }
+                for (int k = 0; k < 10; k++) {
+                    String third = String.valueOf(k);
+                    if(third.equals("3")){
+                        third = "E";
+                    }
+                    System.out.println(first+"-"+second+"-"+third);
+                }
+            }
+        }
     }
     
 }
