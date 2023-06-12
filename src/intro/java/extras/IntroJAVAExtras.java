91ea5ec88f403cb8ebd525767cad2da0707bb9ce
Rebeca Heredia <rbkheredia90@gmail.com>
2023-06-12 18:05:51 -0300

    uso de break

--- src/intro/java/extras/IntroJAVAExtras.java
+++ src/intro/java/extras/IntroJAVAExtras.java
@@ -5,6 +5,8 @@
  */
 package intro.java.extras;
 
+import java.util.Scanner;
+
 /**
  *
  * @author rebek
@@ -15,7 +17,36 @@ public class IntroJAVAExtras {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //ejercicio19
+//        Escriba un programa que averigüe si dos vectores de N enteros son iguales (la
+//comparación deberá detenerse en cuanto se detecte alguna diferencia entre los
+//elementos).
+        Scanner read = new Scanner(System.in);
+        System.out.println("Que cantidad de valores quieres ingresar en cada vector?");
+        int cant = read.nextInt();
+        int vector1[] = new int[cant];
+        int vector2[] = new int[cant];
+        rellenarVector(vector1, cant);
+        rellenarVector(vector2, cant);
+        comparar(vector1, vector2, cant);
     }
-    
+     public static void rellenarVector(int vector[], int q){
+         Scanner read = new Scanner(System.in);
+         System.out.println("Nuevo vector");
+         for (int i = 0; i < q; i++) {
+             System.out.println("Ingrese un valor");
+             vector[i] = read.nextInt();
+         }
+         System.out.println("-----------------");
+     }
+     
+     public static void comparar(int vector1[], int vector2[], int q){
+         for (int i = 0; i < q; i++) {
+             if(vector1[i] != vector2[i]){
+                 System.out.println("Hay una diferencia en el puesto " +(i+1));
+                 break;
+             }
+         }
+         System.out.println("Programa finalizado");
+     }
 }
