08fccd5ab24a6f2db5112a74d2d0b715447f2950
Rebeca Heredia <rbkheredia90@gmail.com>
2023-06-12 18:13:54 -0300

    rellenar y mostrar vector en diferentes subprog

--- src/intro/java/extras/IntroJAVAExtras.java
+++ src/intro/java/extras/IntroJAVAExtras.java
@@ -5,6 +5,8 @@
  */
 package intro.java.extras;
 
+import java.util.Scanner;
+
 /**
  *
  * @author rebek
@@ -15,7 +17,26 @@ public class IntroJAVAExtras {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // ejercicio20
+//        Crear una función rellene un vector con números aleatorios, pasándole un arreglo por
+//parámetro. Después haremos otra función o procedimiento que imprima el vector.
+        System.out.println("Cuantos valores quiere agregar en el vector?");
+        Scanner read = new Scanner(System.in);
+        int q = read.nextInt();
+        int vector[] = new int[q];
+        rellenar(vector, q);
+        mostrar(vector, q);
     }
     
+    public static void rellenar(int vector[], int q){
+        for (int i = 0; i < q; i++) {
+            vector[i] = (int) (Math.random()*15);
+        }
+    }
+    
+    public static void mostrar(int vector[], int q){
+        for (int i = 0; i < q; i++) {
+            System.out.print("["+vector[i]+"]");
+        }
+    }
 }
